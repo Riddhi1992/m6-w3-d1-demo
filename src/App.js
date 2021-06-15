61866b013a5944d47dcfceb58caabc9b4ab06ab3
Riddhi <riddhipatel.csu@gmail.com>
2021-06-15 16:33:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import BarChart from './animatedBar/BarChart';
+import SphereApp from './SphereOfSpheres/SphereApp';
+import TreeApp from './CollapsibleTree/TreeApp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Router>
+      <div className="App">
+        <a className="m-3 btn btn-primary" href="/barChart" rel="noopener noreferrer">
+          Animated Bar Chart
         </a>
-      </header>
-    </div>
+        <a className="m-3 btn btn-primary" href="/spheres" rel="noopener noreferrer">
+          Sphere of Spheres
+        </a>
+        <a className="m-3 btn btn-primary" href="/tree" rel="noopener noreferrer">
+          Collapsible Tree
+        </a>
+
+        <Switch>
+          <Route exact path="/barChart">
+            <BarChart />
+          </Route>
+          <Route exact path="/spheres">
+            <SphereApp />
+          </Route>
+          <Route exact path="/tree">
+            <TreeApp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
